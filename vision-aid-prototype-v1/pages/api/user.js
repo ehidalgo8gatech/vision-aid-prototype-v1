5aa7042ef934bd7dbf9e23d9b3f08a0f28c8f546
S L <sl@Ss-MacBook-Pro.local>
2023-02-26 21:11:47 -0500

    Add More Request Logging

--- vision-aid-prototype-v1/pages/api/user.js
+++ vision-aid-prototype-v1/pages/api/user.js
@@ -1,56 +1,58 @@
-import { PrismaClient } from '@prisma/client';
+import {PrismaClient} from '@prisma/client';
 
 const prisma = new PrismaClient();
 
 export default async function handler(req, res) {
-  if (req.method === 'POST') {
-    return await addData(req, res);
-  } else if (req.method == 'GET'){
-    return await readData(req, res);
-  }
-  else {
-    return res.status(405).json({ message: 'Method not allowed', success: false });
-  }
+    if (req.method === 'POST') {
+        return await addData(req, res);
+    } else if (req.method == 'GET') {
+        return await readData(req, res);
+    } else {
+        return res.status(405).json({message: 'Method not allowed', success: false});
+    }
 }
 
-async function readData(req, res){
-  try{
-    const user = await prisma.user.findUnique({
-      where: {
-        email: req.query.email,
-      },
-      include: {
-          role: true,
-      },
-    })
-    return res.status(200).json(user, {success: true});
-  } catch(error) {
-    console.log(error)
-    return res.status(500).json({error: "Error reading from database", success: false});
-  }
+async function readData(req, res) {
+    try {
+        const user = await prisma.user.findUnique({
+            where: {
+                email: req.query.email,
+            },
+            include: {
+                role: true,
+            },
+        })
+        return res.status(200).json(user, {success: true});
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({error: "Error reading from database", success: false});
+    }
 }
 
 async function addData(req, res) {
-  const body = req.body;
-  try {
-    const newEntry = await prisma.user.create({
-      data: {
-              email: body.email,
-              role: {
-                create:{
-                  dataEntry: body.dataEntry,
-                  admin: body.admin,
-                  manager: body.manager
+    const body = req.body;
+    const create = {
+        data: {
+            email: body.email,
+            role: {
+                create: {
+                    dataEntry: body.dataEntry,
+                    admin: body.admin,
+                    manager: body.manager
                 }
-              }
-            },
-      include: {
-        role: true
-      }
-    })
-    return res.status(200).json(newEntry, { success: true });
-  } catch (error) {
-    console.log('Request error ' + error);
-    res.status(500).json({ error: 'Error adding user' + error, success: false });
-  }
+            }
+        },
+        include: {
+            role: true
+        }
+    }
+    console.log("Request body " + body + "create value " + create)
+
+    try {
+        const newEntry = await prisma.user.create(create)
+        return res.status(200).json(newEntry, {success: true});
+    } catch (error) {
+        console.log('Request error ' + error);
+        res.status(500).json({error: 'Error adding user' + error, success: false});
+    }
 }
