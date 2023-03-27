88650447dc679b6e88f506fd85052eca2a7c7195
mgESW <100160364+mgESW@users.noreply.github.com>
2023-03-26 23:12:03 -0700

    Update index.js

--- vision-aid-prototype-v1/pages/index.js
+++ vision-aid-prototype-v1/pages/index.js
@@ -23,7 +23,7 @@ import Router from 'next/router'
 import readXlsxFile from 'read-excel-file'
 import Head from 'next/head'
 import styles from '@/styles/Home.module.css'
-import Navbar from '../comps/Navbar'
+import Navbar from '@/comps/Navbar'
 
 
 function classNames(...classes) {
