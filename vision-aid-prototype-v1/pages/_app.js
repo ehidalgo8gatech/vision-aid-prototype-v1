100a60664a612b636fbcb9c5001425669a88a559
mgESW <100160364+mgESW@users.noreply.github.com>
2023-03-27 09:27:17 -0700

    Update _app.js

--- vision-aid-prototype-v1/pages/_app.js
+++ vision-aid-prototype-v1/pages/_app.js
@@ -1,6 +1,12 @@
 import '../styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import {SessionProvider} from "next-auth/react"
+import { Inter } from 'next/font/google'
+
+// If loading a variable font, you don't need to specify the font weight
+const inter = Inter({ subsets: ['latin'] })
+
+
 
 export default function App({
                                 Component,
@@ -10,5 +16,6 @@ export default function App({
         <SessionProvider session={session}>
             <Component {...pageProps} />
         </SessionProvider>
+        
     )
 }
