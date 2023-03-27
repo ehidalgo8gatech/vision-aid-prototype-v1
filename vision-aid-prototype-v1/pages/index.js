d5034d9a7936deee495ab814d6d968f2d133aa07
mgESW <100160364+mgESW@users.noreply.github.com>
2023-03-27 09:06:42 -0700

    Update index.js

--- vision-aid-prototype-v1/pages/index.js
+++ vision-aid-prototype-v1/pages/index.js
@@ -1,13 +1,13 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from '@/next/font/google'
+/*import { Inter } from 'next/font/google'*/
 import styles from '@/styles/Home.module.css'
 import {useSession, signIn, signOut, getSession} from "next-auth/react";
 
 /*import Navbar from '../comps/Navbar'*/
 /*import Footer from './footer'*/
 
-const inter = Inter({ subsets: ['latin'] })
+/*const inter = Inter({ subsets: ['latin'] })*/
 
 export default function Home() {
   return (
