1003c4e705885a9d67c52b8eefb13d7ed2612fd2
S L <sl@Ss-MacBook-Pro.local>
2023-03-27 21:42:58 -0400

    Fix Import

--- vision-aid-prototype-v1/pages/_app.js
+++ vision-aid-prototype-v1/pages/_app.js
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.css'
 /*import {SessionProvider} from "next-auth/react"*/
-import { Inter } from 'next/font/google'
+import { Inter } from '@next/font/google'
 
 // If loading a variable font, you don't need to specify the font weight
 const inter = Inter({
