b57463aa5202a455ae5cc481d2012f1dfaa7879d
Suryatej Reddy Vyalla <suryatej16102@iiitd.ac.in>
2023-04-06 19:04:53 -0400

    missed this in prev commit

--- vision-aid-prototype-v1/pages/suryatejbeneficiaryinformation.js
+++ vision-aid-prototype-v1/pages/suryatejbeneficiaryinformation.js
@@ -2,49 +2,9 @@
 
 import React from 'react';
 import { useState } from 'react';
-
-function SearchBar({ onSearch }) {
-    const [searchTerm, setSearchTerm] = useState('');
-  
-    const handleChange = (event) => {
-      setSearchTerm(event.target.value);
-    };
-  
-    const handleSubmit = (event) => {
-      event.preventDefault();
-      onSearch(searchTerm);
-    };
-  
-    return (
-      <form onSubmit={handleSubmit} className="mb-3">
-        <input
-          type="text"
-          value={searchTerm}
-          onChange={handleChange}
-          className="form-control"
-          placeholder="Search users"
-        />
-      </form>
-    );
-  }
-
-  function UserList({ users }) {
-    return (
-      <div className="row">
-        {users.map((user) => (
-          <div key={user.mrn} className="col-md-4 col-sm-6 col-12 mb-4">
-            <div className="card">
-              <div className="card-body">
-                <h5 className="card-title">{user.beneficiaryName}</h5>
-                <p className="card-text">{user.hospital.name}</p>
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
-    );
-  }
-
+import AddUserForm from './components/AddUserForm';
+import SearchBar from './components/SearchBar';
+import UserList from './components/UserList';
 
 function HomePage() {
     const [users, setUsers] = useState([]);
@@ -69,6 +29,7 @@ return (
     <SearchBar onSearch={searchUsers} />
     <UserList users={users} />
     </div>
+    
 );
 }
   
