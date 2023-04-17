afaaeeb4cab4fd804c73a8a84fb7a2354d4ef8a0
Suryatej Reddy Vyalla <suryatej16102@iiitd.ac.in>
2023-04-17 03:02:59 -0400

    fixes navbar

--- vision-aid-prototype-v1/pages/navigation/Navigation.js
+++ vision-aid-prototype-v1/pages/navigation/Navigation.js
@@ -46,8 +46,8 @@ const Navigation = () => {
           list-style: none;
           margin: 0;
           padding: 0;
+          flex-wrap: wrap;
         }
-        
         .logo img {
           height: 80px;
         }
