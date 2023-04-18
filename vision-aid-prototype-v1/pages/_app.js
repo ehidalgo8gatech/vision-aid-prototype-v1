95f85efbd97a7b86253653c1a9eefd808be5c9e8
Suryatej Reddy Vyalla <suryatej16102@iiitd.ac.in>
2023-04-18 19:30:30 -0400

    add boostrap js

--- vision-aid-prototype-v1/pages/_app.js
+++ vision-aid-prototype-v1/pages/_app.js
@@ -2,6 +2,7 @@ import '../styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import {SessionProvider} from "next-auth/react"
 import { Inter } from '@next/font/google'
+import { useEffect } from "react";
 
 // If loading a variable font, you don't need to specify the font weight
 const inter = Inter({
@@ -10,6 +11,10 @@ const inter = Inter({
 })
 
 export default function MyApp({ Component, pageProps }) {
+  // load bootstrap js on useEffect
+  useEffect(() => {
+    require("bootstrap/dist/js/bootstrap.bundle.min.js");
+  }, []);
   return (
       <SessionProvider session={pageProps.session}>
     <main className={`${inter.variable} font-sans`}>
