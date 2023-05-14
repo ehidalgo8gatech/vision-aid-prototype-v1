9361ac9dcdb940700de4fa5ac912ee447058ebdc
Suryatej Reddy Vyalla <suryatej16102@iiitd.ac.in>
2023-05-14 16:35:41 -0400

    cleans up code

--- vision-aid-prototype-v1/pages/reports.js
+++ vision-aid-prototype-v1/pages/reports.js
@@ -12,6 +12,7 @@ import moment from 'moment';
 import { useState, useEffect } from 'react';
 import {findAllBeneficiary} from "@/pages/api/beneficiary";
 import { CSVLink, CSVDownload } from "react-csv";
+import GraphCustomizer from './components/GraphCustomizer';
 
 export async function getServerSideProps(ctx) {
   const session = await getSession(ctx)
@@ -290,45 +291,9 @@ export default function Summary({ user, summary, beneficiaryFlatList }) {
       <Container>
         <h1>Trainings Summary</h1>
         <div className="row">
-          {user.admin != null && (
-            <div className="col-md-2">
-              <Table striped bordered hover>
-                <thead>
-                  <tr>
-                    <th>List of Hospitals</th>
-                    <th>
-                      <button type='button' className='btn btn-light' onClick={handleSelectAll}>Select All</button>
-                    </th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {summary.map((item) => (
-                    <tr key={item.id}>
-                      <td>{item.name}</td>
-                      <td>
-                        <input
-                          type="checkbox"
-                          id={`hospital-${item.id}`}
-                          value={item.id}
-                          onChange={handleHospitalSelection}
-                          checked={selectedHospitals.includes(item.id)}
-                        />
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </Table>
-            </div>
-          )}
+          {user.admin != null && (<GraphCustomizer summary={summary} selectedHospitals={selectedHospitals} handleHospitalSelection={handleHospitalSelection} handleSelectAll={handleSelectAll} startDate={startDate} handleStartDateChange={handleStartDateChange} endDate={endDate} handleEndDateChange={handleEndDateChange} />)}
           <div className="col-md-10">
-            <div>
-              <label htmlFor="startDate">Start Date:</label>
-              <input type="date" id="startDate" name="startDate" value={moment(startDate).format('YYYY-MM-DD')} onChange={handleStartDateChange} />
-              <label htmlFor="endDate">End Date:</label>
-              <input type="date" id="endDate" name="endDate" value={moment(endDate).format('YYYY-MM-DD')} onChange={handleEndDateChange} />
-            </div>
             <Bar data={chartData} />
-            <h4>Total Number of Beneficiaries: {filteredSummary.reduce((sum, item) => sum + item.beneficiary.length, 0)}</h4>
           </div>
         </div>
       </Container>
