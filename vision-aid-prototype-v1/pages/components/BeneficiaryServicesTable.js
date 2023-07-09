86fa4b3d4ce87f664350d4885e2c84c9d4cec949
psingh359 <singhpurva2906@gmail.com>
2023-07-09 17:09:08 -0400

    Creates a new historical evaluation dashboard

---
+++ vision-aid-prototype-v1/pages/components/BeneficiaryServicesTable.js
@@ -0,0 +1,61 @@
+import Link from "next/link";
+import styles from '@/styles/Home.module.css'
+import Head from 'next/head'
+import Image from 'next/image'
+import  { useRouter } from 'next/router';
+import { Inter } from '@next/font/google'
+import {useSession, signIn, signOut, getSession} from "next-auth/react";
+
+
+export default function BeneficiaryServicesTable(props) {
+    const router = useRouter();
+    console.log("props inside bene table: ", props.user.mrn);
+    const openUserHistoricalEvaluationPage = async (mrn = null, service=null) => {
+        console.log("props.mrn, service: ", mrn, service);
+        if (mrn)
+          router.push(`/historicalEvaluation?mrn=${mrn}&service=${service}`);
+        else
+        {
+            router.push(`/beneficiaryinformation`, undefined, {shallow:true});
+        }
+          
+      };
+
+    return(
+        <table class="table beneficiary-table table-bordered">
+        <thead class="thead-dark">
+            <tr>
+            <th scope="col">Services</th>
+            <th scope="col">Evaluation Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+            <th scope="row">Low Vision Screening</th>
+            <td><button type="button" class="btn btn-primary" onClick={() => openUserHistoricalEvaluationPage(props.user.mrn, "Low_Vision_Evaluation")}>History</button><div class="divider"/>
+            <button type="button" class="btn btn-primary">New</button></td>
+            </tr>
+            <tr>
+            <th scope="row">Comprehensive Low Vision Screening</th>
+            <td><button type="button" class="btn btn-primary" onClick={() => openUserHistoricalEvaluationPage(props.user.mrn, "Comprehensive_Low_Vision_Evaluation")}>History</button><div class="divider"/>
+            <button type="button" class="btn btn-primary">New</button></td>
+            </tr>
+            <tr>
+            <th scope="row">Low Enhancement</th>
+            <td><button type="button" class="btn btn-primary" onClick={() => openUserHistoricalEvaluationPage(props.user.mrn, "Vision_Enhancement")}>History</button><div class="divider"/>
+            <button type="button" class="btn btn-primary">New</button></td>
+            </tr>
+            <tr>
+            <th scope="row">Counselling</th>
+            <td><button type="button" class="btn btn-primary" onClick={() => openUserHistoricalEvaluationPage(props.user.mrn, "Counselling_Education")}>History</button><div class="divider"/>
+            <button type="button" class="btn btn-primary">New</button></td>
+            </tr>
+            <tr>
+            <th scope="row">Training</th>
+            <td><button type="button" class="btn btn-primary" onClick={() => openUserHistoricalEvaluationPage(props.user.mrn, "Computer_Training")}>History</button><div class="divider"/>
+            <button type="button" class="btn btn-primary">New</button></td>
+            </tr>
+        </tbody>
+        </table>
+    )
+}
