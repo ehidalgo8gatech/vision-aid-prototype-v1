3c2367dc202e004847238c69e406b7bc1eac7fa1
psingh359 <singhpurva2906@gmail.com>
2023-07-11 11:48:44 -0400

    Correctly handles undefined edge case

--- vision-aid-prototype-v1/pages/components/HistoricalLowVisionScreeningForm.js
+++ vision-aid-prototype-v1/pages/components/HistoricalLowVisionScreeningForm.js
@@ -10,6 +10,7 @@ import {useSession, signIn, signOut, getSession} from "next-auth/react";
 export default function HistoricalLowVisionScreeningForm(props) {
     const data = props.evaluationData;
     return(
+        data == undefined ? <div className="text-align-left">No historical data is present for this date!</div>:
         <table class="table beneficiary-table table-bordered">
         <thead class="thead-dark">
             <tr>
