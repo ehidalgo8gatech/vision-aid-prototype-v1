7de4b88aecd6fb9b014e59920ea59d3b77cec4e3
Rucha Sathe <imrucha98@gmail.com>
2023-09-07 17:03:26 -0400

    minor typo removed

--- vision-aid-prototype-v1/pages/api/counsellingType.js
+++ vision-aid-prototype-v1/pages/api/counsellingType.js
@@ -50,7 +50,7 @@ async function deleteData(req, res) {
 async function updateData(req, res) {
 
 }
-g
+
 async function readData(req, res) {
     const counseling = await prisma.counselling_Type.findMany({});
     return res.status(200).json(counseling, {success: true});
