3fa8e82bde42d9951984881967f73bbdd3f687e0
Rucha Sathe <imrucha98@gmail.com>
2023-11-13 15:08:39 -0500

    Change display of logged in user

--- vision-aid-prototype-v1/pages/navigation/Navigation.js
+++ vision-aid-prototype-v1/pages/navigation/Navigation.js
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { signIn, signOut} from "next-auth/react";
+import { signIn, signOut } from "next-auth/react";
 
 function Navigation({ user } = props) {
   const router = useRouter();
@@ -9,110 +9,133 @@ function Navigation({ user } = props) {
     role = user.admin
       ? "admin"
       : user.hospitalRole.admin
-        ? "manager"
-        : user.hospitalRole
-          ? "technician"
-          : "invalid";
+      ? "manager"
+      : user.hospitalRole
+      ? "technician"
+      : "invalid";
   }
 
   return (
     <nav
-      className="navbar navbar-expand-lg navbar-dark"
+      className="navbar navbar-expand-lg navbar-dark container-fluid flex"
       style={{ backgroundColor: "#1B5E20" }}
     >
-      <div className="container-fluid">
-        <Link href="/" legacyBehavior>
-          <a className="navbar-brand p-2">
-            <img
-              src="/vision-aid-logo.jpeg"
-              alt="Logo"
-              height="80"
-              width="80"
-            />
-          </a>
-        </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        {user && role != "invalid" && (
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-              <li className="nav-item p-4">
-                <Link href="/beneficiary" legacyBehavior>
-                  <a
-                    className={`nav-link custom-link ${
-                      router.pathname === "/beneficiary" ? "active" : ""
-                    }`}
-                  >
-                    Beneficiaries
-                  </a>
-                </Link>
-              </li>
-              <li className="nav-item p-4">
-                <Link href="/reports" legacyBehavior>
-                  <a
-                    className={`nav-link custom-link ${
-                      router.pathname === "/reports" ? "active" : ""
-                    }`}
-                  >
-                    Reports
-                  </a>
-                </Link>
-              </li>
-              {/* don't display if technician */}
-              {(role === "admin" || role === "manager") && (
+      {/* <div className="container-fluid flex"> */}
+      <div className="d-flex w-100">
+        <div className="p-2">
+          <Link href="/" legacyBehavior>
+            <a className="navbar-brand p-2">
+              <img
+                src="/vision-aid-logo.jpeg"
+                alt="Logo"
+                height="80"
+                width="80"
+              />
+            </a>
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+        </div>
+        <div>
+          {user && role != "invalid" && (
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <ul className="navbar-nav">
                 <li className="nav-item p-4">
-                  <Link href="/users" legacyBehavior>
+                  <Link href="/beneficiary" legacyBehavior>
                     <a
                       className={`nav-link custom-link ${
-                        router.pathname === "/users" ? "active" : ""
+                        router.pathname === "/beneficiary" ? "active" : ""
                       }`}
                     >
-                      Users
+                      Beneficiaries
                     </a>
                   </Link>
                 </li>
-              )}
-              {/* display only if admin */}
-              {role === "admin" && (
                 <li className="nav-item p-4">
-                  <Link href="/requiredfields" legacyBehavior>
+                  <Link href="/reports" legacyBehavior>
                     <a
                       className={`nav-link custom-link ${
-                        router.pathname === "/requiredfields" ? "active" : ""
+                        router.pathname === "/reports" ? "active" : ""
                       }`}
                     >
-                      Configuration
+                      Reports
                     </a>
                   </Link>
                 </li>
-              )}
-            </ul>
-          </div>
-        )}
-
+                {/* don't display if technician */}
+                {(role === "admin" || role === "manager") && (
+                  <li className="nav-item p-4">
+                    <Link href="/users" legacyBehavior>
+                      <a
+                        className={`nav-link custom-link ${
+                          router.pathname === "/users" ? "active" : ""
+                        }`}
+                      >
+                        Users
+                      </a>
+                    </Link>
+                  </li>
+                )}
+                {/* display only if admin */}
+                {role === "admin" && (
+                  <li className="nav-item p-4">
+                    <Link href="/requiredfields" legacyBehavior>
+                      <a
+                        className={`nav-link custom-link ${
+                          router.pathname === "/requiredfields" ? "active" : ""
+                        }`}
+                      >
+                        Configuration
+                      </a>
+                    </Link>
+                  </li>
+                )}
+              </ul>
+            </div>
+          )}
+        </div>
         {user && (
-          <div className="text-align-right">
-            <p className="display text-light nopadding">
+          <div className="left-auto-margin column">
+            {/* <p> */}
+            <small className="top display text-light">
               Signed in as {user.email}
+            </small>
+            {/* </p> */}
+            <br />
+            <div className="text-align-right">
+              <button
+                type="button"
+                className="btn btn-sm btn-light"
+                onClick={() => {
+                  signOut();
+                }}
+              >
+                Sign out
+              </button>
+            </div>
+            {/* <p className="text-align-right display text-light nopadding">
+              <small>Signed in as {user.email}</small>
             </p>
-            <button
-              type="button"
-              className="btn btn-sm btn-light"
-              onClick={() => {
-                signOut();
-              }}
-            >
-              Sign out
-            </button>
+            <div className="h-100 right-align nopadding">
+              <button
+                type="button"
+                className="btn btn-sm btn-light"
+                onClick={() => {
+                  signOut();
+                }}
+              >
+                Sign out
+              </button>
+            </div> */}
           </div>
         )}
         {!user && (
@@ -126,7 +149,9 @@ function Navigation({ user } = props) {
             </button>
           </div>
         )}
+        {/* </div> */}
       </div>
+
       <style jsx>{`
         .custom-link {
           font-size: 1.5rem;
