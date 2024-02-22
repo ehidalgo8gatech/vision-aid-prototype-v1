83cb544fe3506e4476ee604c905b12153ceb4303
Nasa Quba <nasa@oath.com>
2024-02-21 17:25:55 -0800

    Prototype_1

---
+++ vision-aid-prototype-v1/pages/ProtoBox.js
@@ -0,0 +1,12 @@
+function ProtoBox({ user } = props) {
+    return (
+        <div>
+            <br></br>
+            <div> <b> Vision-Aid and OMSCS collaboration</b></div>
+            <br></br>
+            <div> More content will go here </div>
+        </div>
+    );
+}
+
+export default ProtoBox;
