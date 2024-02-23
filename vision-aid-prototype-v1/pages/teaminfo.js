9eabacf197563615c154fa1eadd78b0c6d46ba66
amolina38 <159068654+amolina38@users.noreply.github.com>
2024-02-23 17:46:14 -0500

    Update teaminfo.js

--- vision-aid-prototype-v1/pages/teaminfo.js
+++ vision-aid-prototype-v1/pages/teaminfo.js
@@ -41,7 +41,7 @@ export default function TeamInfo() {
           <ol>
             <li>Rewrite authentication system to be more seamless</li>
             <li>Integrate authorization into the authentication system</li>
-            <li>Design a customizable landing page that doesn't require dev assistance</li>
+            <li>Design a customizable landing page that doesn&apos;t require dev assistance</li>
             <li>Upgrade the UI to meet accessibility standards</li>
             <li>Implement a feedback form for users to report issues and bugs</li>
             <li>Add a footer to all pages</li>
