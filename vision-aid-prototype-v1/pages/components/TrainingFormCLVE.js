a8a5f7881bf6ba3ee74c51b57258d15ede09dbf7
Vedant Mahulkar <vedant.mahulkar@gmail.com>
2024-03-04 13:57:50 -0500

    make default field for distance acuity information blank

--- vision-aid-prototype-v1/pages/components/TrainingFormCLVE.js
+++ vision-aid-prototype-v1/pages/components/TrainingFormCLVE.js
@@ -17,6 +17,7 @@ import {
   mUnitsValues,
   snellenImperialValues,
   snellenMetricValues,
+  blankField,
 } from "../../constants/acuityConstants";
 import {
   spectacleDevices,
@@ -85,7 +86,8 @@ const TrainingFormCLVE = ({
     e.preventDefault();
     console.log(formData);
     const customDataDistance = customFieldsDistance.reduce((acc, field) => {
-      if(formData[field] === otherField) {
+      if(formData[field] === otherField || formData[field] === blankField) {
+        console.log(formData[field]);
         acc[field] = formData[field];
       } else {
         acc[field] = formData[field] + " " + formData["unitDistance"];
@@ -93,7 +95,7 @@ const TrainingFormCLVE = ({
       return acc;
     }, {});
     const customDataNear = customFieldsNear.reduce((acc, field) => {
-      if(formData[field] === otherField) {
+      if(formData[field] === otherField || formData[field] === blankField) {
         acc[field] = formData[field];
       } else {
         acc[field] = formData[field] + " " + formData["unitNear"];
