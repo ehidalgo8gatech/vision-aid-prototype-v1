85750b42d4a9d71c1e71aa9498e30fbe6374e5a0
Nasa Quba <nasa@yahooinc.com>
2024-03-22 23:33:02 -0700

    crud and tests

--- pages/api/landingPage.test.js
+++ pages/api/landingPage.test.js
@@ -2,10 +2,11 @@ const axios = require('axios');
 const apiUrl = 'http://localhost:3000/api/landingPage'; 
 
 describe('UserContent handler', () => {
+    let uid = 2;
     test('should add content successfully', async () => {
         // Mock request body
         const requestBody = {
-            userId: 2,
+            userId: uid,
             content: 'New content'
         };
         try {
@@ -17,28 +18,25 @@ describe('UserContent handler', () => {
         }
     });
     test('should read content successfully', async () => {
+        // Mock request body
         var requestData = {
-            userId: 2,
+            userId: uid,
         };
         const queryString = new URLSearchParams(requestData).toString();
         try {
             // Send a GET request to the API endpoint /api/landingPage?userId=2
             const response = await axios.get(`${apiUrl}?${queryString}`);
-            // console.log("reponse.data got it - ", response)
+            expect(response.status).toBe(200);
             if (response.data == null ) {
                 expect(response.data.userId).toBe(requestData.userId)
-            } else {
-                expect(response.data.userId).toBe(requestData.userId)
             }
-            expect(response.status).toBe(200);
         } catch (error) {
             throw new Error(error);
         }
     });
     test('should update content successfully', async () => {
-        // Mock request body
         const requestBody = {
-            userId: 2,
+            userId: uid,
             content: 'New updated content'
         };
         try {
@@ -50,22 +48,20 @@ describe('UserContent handler', () => {
         }
     });
     test('should delete content successfully', async () => {
+        // Mock request body
         var requestData = {
-            userId: 2,
+            userId: uid,
         };
         const queryString = new URLSearchParams(requestData).toString();
         try {
-            // Send a DELETE request to the API endpoint
+            // Send a DELETE request to the API endpoint /api/landingPage?userId=2
             const response = await axios.delete(`${apiUrl}?${queryString}`);
+            expect(response.status).toBe(200);
             if (response.data == null ) {
-                expect(response.data.userId).toBe(requestData.userId)
-            } else {
                 expect(response.data.userId).toBe(requestData.userId)
             }
         } catch (error) {
             throw new Error(error);;
         }
     });
-
-
 });
