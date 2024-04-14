ad827a1bd8dedad57fa9f2c8c54404f43a805dfc
Austin Bieber <ajbieber1995@gmail.com>
2024-04-14 11:05:32 -0600

    Removed duplicate function from merge conflict

--- pages/api/landingPage.js
+++ pages/api/landingPage.js
@@ -17,16 +17,6 @@ export default async function handler(req, res) {
   }
 }
 
-export async function findAllLandingPagePosts() {
-  const posts = await prisma.landing_Page.findMany();
-  return posts.map(post => {
-    return {
-      ...post,
-      creationDate: new Date(post.creationDate).getTime(),
-    };
-  });
-}
-
 async function updateContent(req, res) {
   let  updatedUser
   try {
