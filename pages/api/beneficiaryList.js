96626186b0a3a918e05b00164b4c8948980a0f7b
Justin McLellan <justin.mclellan99@gmail.com>
2024-07-17 22:12:43 -0500

    update where for beneficiaryList

--- pages/api/beneficiaryList.js
+++ pages/api/beneficiaryList.js
@@ -61,16 +61,17 @@ async function fetchData(req, res) {
                 Orientation_Mobility_Training: true,
             },
             where: {
-                deleted: false,
-                hospitalId: { equals: id },
-                Vision_Enhancement: { every: { date } },
-                Counselling_Education: { every: { date } },
-                Comprehensive_Low_Vision_Evaluation: { every: { date } },
-                Low_Vision_Evaluation: { every: { date } },
-                Training: { every: { date } },
-                Computer_Training: { every: { date } },
-                Mobile_Training: { every: { date } },
-                Orientation_Mobility_Training: { every: { date } }
+                deleted: false, hospitalId: { equals: id },
+                OR: [{
+                    Vision_Enhancement: { every: { date } },
+                    Counselling_Education: { every: { date } },
+                    Comprehensive_Low_Vision_Evaluation: { every: { date } },
+                    Low_Vision_Evaluation: { every: { date } },
+                    Training: { every: { date } },
+                    Computer_Training: { every: { date } },
+                    Mobile_Training: { every: { date } },
+                    Orientation_Mobility_Training: { every: { date } }
+                }]
             },
         });
 
